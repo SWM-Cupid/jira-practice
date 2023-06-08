7a949645113c47fa987264a3120f852f613f87f1
smc2315 <choeyongtae085@gmail.com>
2023-06-08 14:43:13 +0900

    feat: MemberService 추가

---
+++ backend/src/main/java/com/cupid/backend/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.cupid.backend.member.service;
+
+import com.cupid.backend.member.controller.dto.SignUpRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+
+    public void create(SignUpRequest signUpRequest) {
+
+    }
+}

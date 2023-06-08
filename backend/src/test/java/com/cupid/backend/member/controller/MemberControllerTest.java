dac74c83a3ac529c24650d377fea74020d0ec9cd
smc2315 <choeyongtae085@gmail.com>
2023-06-08 14:42:55 +0900

    refactor: MemberRequest -> SignUpRequest 변경

--- backend/src/test/java/com/cupid/backend/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/cupid/backend/member/controller/MemberControllerTest.java
@@ -1,7 +1,8 @@
 package com.cupid.backend.member.controller;
 
-import com.cupid.backend.post.controller.dto.PostRequest;
-import com.cupid.backend.post.service.PostService;
+import com.cupid.backend.ApiDocument;
+import com.cupid.backend.member.controller.dto.SignUpRequest;
+import com.cupid.backend.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(MemberController.class)
-public class MemberControllerTest {
+public class MemberControllerTest extends ApiDocument {
 
     private SignUpRequest signUpRequest;
 
@@ -32,7 +33,7 @@ public class MemberControllerTest {
     @Test
     void 회원가입_성공() throws Exception {
         // given
-        willDoNothing().given(signUpRequest).create(any(SignUpRequest.class));
+        willDoNothing().given(memberService).create(any(SignUpRequest.class));
         // when
         ResultActions resultActions = 회원가입_요청();
         // then
@@ -40,14 +41,14 @@ public class MemberControllerTest {
     }
 
     private ResultActions 회원가입_요청() throws Exception {
-        return mockMvc.perform(post("/api/v1/posts")
+        return mockMvc.perform(post("/api/v1/members")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(toJson(postRequest)));
+                .content(toJson(signUpRequest)));
     }
 
     private ResultActions 회원가입_요청_성공(ResultActions resultActions) throws Exception {
         return resultActions.andExpect(status().isOk())
                 .andDo(print())
-                .andDo(toDocument("create-post-success"));
+                .andDo(toDocument("create-member-success"));
     }
 }
